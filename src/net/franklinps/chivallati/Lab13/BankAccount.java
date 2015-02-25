9109af82625c9aa9b3c014285ef559593b529fda
Tyler <Tyler@HS-19485>
2015-02-25 13:44:24 -0500

    Added serialization. All seems to be working.
    Need to implement PIN/encryption.

--- src/net/franklinps/chivallati/Lab13/BankAccount.java
+++ src/net/franklinps/chivallati/Lab13/BankAccount.java
@@ -1,5 +1,7 @@
 package net.franklinps.chivallati.Lab13;
 
+
+import java.io.*;
 /**
  * Created by CompSci-04 on 2/23/2015.
  */
@@ -22,10 +24,22 @@ public class BankAccount implements AccountsInterface
 	public BankAccount( double balance , String firstName , String lastName )
 	{
 
-		this.balance = balance;
-		setAccountID();
-		setName( firstName , lastName );
-
+		try(
+			      InputStream file = new FileInputStream(lastName + ", " + firstName + ".account");
+			      InputStream buffer = new BufferedInputStream(file);
+			      ObjectInput input = new ObjectInputStream (buffer);
+			    ){
+			      String[] recoveredAccount = (String[])input.readObject();
+					this.balance = Double.parseDouble(recoveredAccount[1]);
+					accountID = Long.valueOf(recoveredAccount[0]);
+					setName( firstName , lastName );
+			    }
+			    catch(Throwable ex){ // Catches all possible exceptions, which will then assume account is new and will be created below.
+					this.balance = balance; 
+					setAccountID();
+					setName( firstName , lastName );
+					saveAccount();
+			    }
 	}
 
 	public double getBalance() { return balance; }
@@ -34,14 +48,14 @@ public class BankAccount implements AccountsInterface
 	{
 
 		this.balance += amount;
-
+		saveAccount();
 	}
 
 	public void withdraw( double amount )
 	{
 
 		this.balance -= amount;
-
+		saveAccount();
 	}
 
 	private void setAccountID()
@@ -55,7 +69,7 @@ public class BankAccount implements AccountsInterface
 	{
 
 		name = lastName + ", " + firstName;
-
+		
 	}
 
 	public String toString()
@@ -63,6 +77,24 @@ public class BankAccount implements AccountsInterface
 
 		return "[ " + "$" + ( Math.round( balance * 100.0 ) / 100.0 ) + ", " + accountID + ", " + name + " ]";
 
+	}
+	
+	private void saveAccount()
+	{
+		String[] accountData = new String[3];
+		accountData[0] = Long.toString(accountID);
+		accountData[1] = Double.toString(balance);
+		accountData[2] = name;
+		 try (
+			      OutputStream file = new FileOutputStream(name + ".account");
+			      OutputStream buffer = new BufferedOutputStream(file);
+			      ObjectOutput output = new ObjectOutputStream(buffer);
+			    ){
+			      output.writeObject(accountData);
+			    }  
+			    catch(IOException ex){
+			      System.out.println("Couldn't save file!");
+			    }
 	}
 
 }
